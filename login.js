0a5a8df674b0aa680a327e7f78685e7af0b94ed5
Praveenraj <104712499+Praveenraj59@users.noreply.github.com>
2023-04-06 19:36:41 +0530

    update

--- login.js
+++ login.js
@@ -1,3 +1,38 @@
+function validateForm() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if (username == "") {
+      alert("Please enter a username.");
+      return false;
+  }
+  if (password == "") {
+      alert("Please enter a password.");
+      return false;
+  }
+}
+function validateForm1() {
+  var username = document.getElementById("username").value;
+  var tel = document.getElementById("tel").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  if (username == "") {
+      alert("Please enter a Username.");
+      return false;
+  }
+  if (tel == "") {
+    alert("Please enter a Phone no.");
+    return false;
+}
+  if (email == "") {
+      alert("Please enter a Email.");
+      return false;
+  }
+  if (password == "") {
+      alert("Please enter a password.");
+      return false;
+  }
+}
+
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
