f756ebe0b0dcce947a805e321655e67cef4e20f9
Praveenraj <104712499+Praveenraj59@users.noreply.github.com>
2023-04-14 22:58:10 +0530

    UI update

--- nearby.js
+++ nearby.js
@@ -2,3 +2,9 @@ function takevalue(){
     var Hospital=document.getElementById("Hospital").value;
 
 }
+
+var value = 42;
+document.getElementById("result").innerHTML = "The value is " + value;
+
+var value = 42;
+localStorage.setItem("value", value);
