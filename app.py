d9a39c5b2213aad19a679b22f9bd3589277fe264
Praveenraj <104712499+Praveenraj59@users.noreply.github.com>
2023-04-15 20:10:50 +0530

    DB

--- app.py
+++ app.py
@@ -1,38 +1,28 @@
-from flask import Flask, render_template,request,flash,redirect,url_for,session
+from flask import Flask, render_template, request, redirect
 import sqlite3
-
 app = Flask(__name__)
-app.secret_key="123"
+ 
 
-con=sqlite3.connect("database.db")
-con.execute("create table if not exists customer(name text primary key,phone integer,email text,)")
-con.close()
+@app.route('/',methods=['GET','POST'])
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/login',methods=['GET','POST'])
-def register():
-    if request.method=='POST':
-        try:
-            name=request.form['name']
-            contact=request.form['contact']
-            mail=request.form['mail']
-            con=sqlite3.connect("database.db")
-            cur=con.cursor()
-            cur.execute("insert into customer(name,contact,mail)values(?,?,?)",(name,contact,mail))
-            con.commit()
-            flash("Record Added  Successfully","success")
-        except:
-            flash("Error in Insert Operation","danger")
-        finally:
-            return redirect(url_for("index"))
-            con.close()
-
+def login():
+    print("1")
+    if request.method == 'POST':
+        print("2")
+        name = request.form['email']
+        print("3")
+        password = request.form['password']
+        connection = sqlite3.connect('database.db')
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO user (email,password) VALUES (?,?)",(name,password))
+        connection.commit()
+        connection.close()
+        print("Data Added successfully")
+        #return redirect('/index1')
+        return render_template('user.html')
     return render_template('login.html')
 
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for("index"))
+
+if __name__ == '__main__':
+ 
+    app.run()
